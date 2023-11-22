2f4b338984ca52364e5ba43fa73947546110c2c3
csh0101 <1945598041@qq.com>
2023-11-22 16:23:57 +0800

    chore: Logger use refactor,static maybe more good

--- src/NearsLog/Logger.php
+++ src/NearsLog/Logger.php
@@ -3,41 +3,124 @@
 namespace NearsLog;
 
 use Monolog\Formatter\JsonFormatter;
-use Monolog\Logger as MonologLogger;
+use Monolog\Logger as MonoLogger;
 use Monolog\Handler\StreamHandler;
 
 use Monolog\ErrorHandler;
 
-
-class LogLevel
-{
-    public const DEBUG = 100;
-    public const INFO = 200;
-}
-
 class Logger
 {
-    protected $logger;
 
-    protected $bizName;
+    public const DEBUG = 100;
+
+    /**
+     * Interesting events
+     *
+     * Examples: User logs in, SQL logs.
+     */
+    public const INFO = 200;
+
+    /**
+     * Uncommon events
+     */
+    public const NOTICE = 250;
+
+    /**
+     * Exceptional occurrences that are not errors
+     *
+     * Examples: Use of deprecated APIs, poor use of an API,
+     * undesirable things that are not necessarily wrong.
+     */
+    public const WARNING = 300;
+
+    /**
+     * Runtime errors
+     */
+    public const ERROR = 400;
+    private static $loggerInstance;
+    private static $bizName;
+    private static $loggerName;
+    private static $logFilePath;
+    private static $logLevel;
 
 
-    public function __construct($bizName = "application", $loggerName, $logFilePath, $logLevel = MonologLogger::DEBUG)
+    private function __construct()
     {
-        $this->bizName = $bizName;
-        $this->logger = new MonologLogger($loggerName);
+    }
+
+    public static function setConfig($bizName, $loggerName, $logFilePath, $logLevel)
+    {
+        self::$bizName = $bizName;
+        self::$loggerName = $loggerName;
+        self::$logFilePath = $logFilePath;
+        self::$logLevel = $logLevel;
+    }
+
+    public static function getLogFilePath()
+    {
+        return self::$logFilePath;
+    }
+
+    public static function getBizName()
+    {
+        return self::$bizName;
+    }
+
+    public static function getLoggerName()
+    {
+        return self::$loggerName;
+    }
+
+
+    /**
+        @reurn MonoLogger
+     **/
+    public static function getLogger()
+    {
+        if (self::$loggerInstance == null) {
+            self::$loggerInstance = self::createLoggerInstance();
+        }
+
+        return self::$loggerInstance;
+    }
+
+    private static function createLoggerInstance()
+    {
+        $logLevel = self::$logLevel ?: MonoLogger::DEBUG;
+        $logger = new MonoLogger(self::$loggerName ?: 'default');
         $formatter = new JsonFormatter();
-        $file_handler = new StreamHandler($logFilePath, $logLevel);
+        $file_handler = new StreamHandler(self::$logFilePath ?: '/tmp/biz.log', $logLevel);
         $file_handler->setFormatter($formatter);
-        ErrorHandler::register($this->logger, [], [], null);
-        $this->logger->pushHandler($file_handler);
-        $this->logger->pushProcessor(function ($record) {
-            $record['extra']['biz_name'] = $this->bizName;
+        $logger->pushHandler($file_handler);
+        $logger->pushProcessor(function ($record) {
+            $record['extra']['biz_name'] = self::$bizName ?: 'default';
             return $record;
         });
+        return $logger;
     }
-    public function log(int $level, string $message, array $context = [])
+
+    public static function debug(string $message, array $context = [])
     {
-        $this->logger->log($level, $message, $context);
+        self::getLogger()->debug($message, $context);
+    }
+
+    public static function notice(string $message, array $context = [])
+    {
+        self::getLogger()->notice($message, $context);
+    }
+
+    public static function warning(string $message, array $context = [])
+    {
+        self::getLogger()->warning($message, $context);
+    }
+
+    public static function error(string $message, array $context = [])
+    {
+        self::getLogger()->error($message, $context);
+    }
+
+    public static function info(string $message, array $context = [])
+    {
+        self::getLogger()->info($message, $context);
     }
 }
--- src/Test/LoggerTest.php
+++ src/Test/LoggerTest.php
@@ -2,31 +2,25 @@
 
 use PHPUnit\Framework\TestCase;
 use NearsLog\Logger;
-use NearsLog\LogLevel;
 
 class NearsLoggerTest extends TestCase
 {
-    public function testLogMethod()
+
+    public function testDefaultLogger()
     {
-        $bizName = 'MyBiz';
-        $loggerName = 'MyLogger';
-        $logFilePath = '/tmp/log_test.log';
+        $message = "a test log";
+        $context = ["user" => "nears"];
+        $logFilePath = "/tmp/biz.log";
+        $bizName = "default";
+        $loggerName = "default";
 
-        // 创建一个 Logger 实例
-        $logger = new Logger($bizName,$loggerName, $logFilePath);
+        Logger::setConfig($bizName, $loggerName, $logFilePath, Logger::INFO);
+        Logger::Info($message, $context);
 
-        // 调用 log 方法记录一条日志
-        $message = 'Test log message';
-        $level = LogLevel::INFO;
-        $context = ['user_id' => 123,'user_name'=>'test'];
-        $logger->log($level, $message, $context);
-
-        // 在这里添加断言来验证日志是否成功记录，例如检查日志文件是否包含预期的内容
-        // 检查日志文件是否包含了你刚才记录的日志内容
         $logContent = file_get_contents($logFilePath);
         $this->assertStringContainsString($message, $logContent);
-        $this->assertStringContainsString($loggerName, $logContent);
         $this->assertStringContainsString($bizName, $logContent);
+        $this->assertStringContainsString($loggerName, $logContent);
         // 更多可能的断言...
     }
 }
